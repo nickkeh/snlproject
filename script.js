7a8568198cc426cb6949f070cd4e825c91a786a0
susantikcm <susantikcm@yahoo.com>
2020-09-11 16:23:22 +1000

    add validateTaskForm function

--- script.js
+++ script.js
@@ -1,49 +1,57 @@
-document.querySelector('#due-date').value = new Date().toISOString().substring(0, 10);
-
 /************************Validate input************************/
 // Name -> Not Empty and longer than 8 characters
 // Description -> Not Empty and longer than 15 characters
 // AssignedTo -> Not Empty and longer than 8 characters
 // DueDate  -> Not Empty and not in the past
 
+document.querySelector('#due-date').value = new Date().toISOString().substring(0, 10);
+
+const toggleValid = (valid, element) => {
+    if(valid) {
+        element.classList.add('is-valid');
+        element.classList.remove('is-invalid');
+    }
+    else {
+        element.classList.add('is-invalid');
+        element.classList.remove('is-valid');
+    }
+}
+
+document.querySelector('#due-date').addEventListener('change', (event) => {
+    event.preventDefault();
+    const dueDate = document.querySelector('#due-date').value;
+    const today = new Date().toISOString().substring(0, 10);
+    const isDueDateValid = dueDate > today;
+    toggleValid(isDueDateValid, dueDate);
+}, true);
+
 const validateTaskForm = () => {
-    let isValid = false;
     const name = document.querySelector('#name');
+    const isNameValid = name.value.length !== 0 && name.value.length <= 8;
+    toggleValid(isNameValid, name);
+
     const description = document.querySelector('#description');
+    const isDescriptionValid = description.value.length !== 0 && description.value.length <= 15;
+    toggleValid(isDescriptionValid, description);
+
     const assignedTo = document.querySelector('#assigned-to');
+    const isAssignedToValid = assignedTo.selectedIndex > 0 && assignedTo.options[assignedTo.selectedIndex].value.length <= 8;
+    toggleValid(isAssignedToValid, assignedTo);
 
-    if(name.value.length !== 0 && name.value.length <= 8) {
-        name.classList.add('is-valid');
-        name.classList.remove('is-invalid');
-    }
-    else {
-        name.classList.add('is-invalid');
-        name.classList.remove('is-valid');
-    }
+    const dueDate = document.querySelector('#due-date');
+    const today = new Date().toISOString().substring(0, 10);
+    const isDueDateValid = dueDate.value > today;
+    toggleValid(isDueDateValid, dueDate);
 
-    if(description.value.length !== 0 && description.value.length <= 15) {
-        description.classList.add('is-valid');
-        description.classList.remove('is-invalid');
-    }
-    else {
-        description.classList.add('is-invalid');
-        description.classList.remove('is-valid');
-    }
-
-    if(assignedTo.selectedIndex > 0 && assignedTo.options[assignedTo.selectedIndex].value.length <= 8)  {
-        assignedTo.classList.add('is-valid');
-        assignedTo.classList.remove('is-invalid');
-    }
-    else {
-        assignedTo.classList.add('is-invalid');
-        assignedTo.classList.remove('is-valid');
-    }
+    if(isNameValid && isDescriptionValid && isAssignedToValid && isDueDateValid) 
+        return true;
+    else
+        return false;
 }
 
 document.querySelector('#create-task').addEventListener('submit', (event) => {
     event.preventDefault();
-    // console.log('this is submit');
-    validateTaskForm();
+    console.log(validateTaskForm());
     // const item = document.createElement('li');
     // item.innerText = document.querySelector('#form-hobby-text').value;
     // item.classList.add('list-group-item');
@@ -52,18 +60,9 @@ document.querySelector('#create-task').addEventListener('submit', (event) => {
     // document.querySelector('#form-hobby-text').value = "";
 }, true);
 
-let dueDate = document.querySelector('#due-date');
-dueDate.addEventListener('change', (event) => {
-    event.preventDefault();
-   
-    const pickedDate = dueDate.value;
-    const today = new Date().toISOString().substring(0, 10);;
-    if(pickedDate >= today) {
-        dueDate.classList.add('is-valid');
-        dueDate.classList.remove('is-invalid');
+const status = document.querySelector('#status');
+status.addEventListener('change', () => {
+    const selectedStatus = status.options[status.selectedIndex].value;
+    switch (selectedStatus) {
     }
-    else {
-        dueDate.classList.add('is-invalid');
-        dueDate.classList.remove('is-valid');
-    }
- }, true);
+});
