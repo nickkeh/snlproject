d0c6874016fe9360df5e9a4fe535a8bc0d2b8818
susantikcm <susantikcm@yahoo.com>
2020-09-17 09:16:44 +1000

    Add Task 6-7

---
+++ js/index.js
@@ -0,0 +1,164 @@
+//Create navbar for every pages
+let navbar = document.querySelector('nav');
+if(navbar) {
+    navbar.innerHTML = 
+    '<a href="#" class="navbar-brand d-none d-md-block">snl-project</a>' +
+    '<button class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">' +
+        '<span class="navbar-toggler-icon"></span>' +
+    '</button>' +
+    '<div class="collapse navbar-collapse" id="navbarCollapse">' +
+        '<ul class="navbar-nav mr-auto">' +
+            '<li class="navbar-item">' +
+                '<a href="./index.html" class="nav-link">Home</a>' +
+            '</li>' +
+            '<li class="navbar-item">' +
+                '<a href="./create-task.html" class="nav-link">Create Tasks</a>' +
+            '</li>' +
+            '<li class="navbar-item">' +
+                '<a href="./contact-us.html" class="nav-link">Contact Us</a>' +
+            '</li>' +
+        '</ul>' +
+    '</div>' +
+    '<form class="form-inline d-md-block d-none my-2 my-lg-0">' +
+        '<input class="form-control mr-sm-2 py-0" type="search" placeholder="Search" aria-label="Search">' +
+        '<button class="btn btn-sm btn-outline-light my-2 my-sm-0" type="submit">Search</button>' +
+    '</form>' +
+    '<button class="btn btn-sm rounded-circle d-block d-md-none text-white border ml-2">+</button>';
+    navbar.classList.add('navbar', 'navbar-dark', 'bg-dark', 'navbar-expand-lg', 'fixed-top', 'py-0', 'mb-auto');
+}
+
+/************************************ Task 4 ************************************/
+/******************************* Input Validation *******************************/
+// #region
+
+// Name -> Not Empty and longer than 8 characters
+// Description -> Not Empty and longer than 15 characters
+// AssignedTo -> Not Empty and longer than 8 characters
+// DueDate  -> Not Empty and not in the past
+
+// set due date input to display today's date 
+if(document.querySelector('#due-date')) {
+    document.querySelector('#due-date').value = new Date().toISOString().substring(0, 10);
+}
+
+// method that sets the valid or invalid style to input element based on input validity
+const toggleValid = (valid, element) => {
+    if(valid) {
+        element.classList.add('is-valid');
+        element.classList.remove('is-invalid');
+    }
+    else {
+        element.classList.add('is-invalid');
+        element.classList.remove('is-valid');
+    }
+};
+
+// validate the duedate input when onchange   
+if(document.querySelector('#due-date')) {
+    document.querySelector('#due-date').addEventListener('change', (event) => {
+        event.preventDefault();
+        const dueDate = document.querySelector('#due-date');
+        const today = new Date().toISOString().substring(0, 10);
+        const isDueDateValid = dueDate.value > today;
+        toggleValid(isDueDateValid, dueDate);
+    }, true);
+}
+
+// validate user input on create task form
+const validateTaskForm = () => {
+    const name = document.querySelector('#name');
+    const isNameValid = name.value.length !== 0 && name.value.length <= 8;
+    toggleValid(isNameValid, name);
+
+    const description = document.querySelector('#description');
+    const isDescriptionValid = description.value.length !== 0 && description.value.length <= 15;
+    toggleValid(isDescriptionValid, description);
+
+    const assignedTo = document.querySelector('#assigned-to');
+    const isAssignedToValid = assignedTo.selectedIndex > 0 && assignedTo.options[assignedTo.selectedIndex].value.length <= 8;
+    toggleValid(isAssignedToValid, assignedTo);
+
+    const dueDate = document.querySelector('#due-date');
+    const today = new Date().toISOString().substring(0, 10);
+    const isDueDateValid = dueDate.value > today;
+    toggleValid(isDueDateValid, dueDate);
+
+    const status = document.querySelector('#status');
+
+    if(isNameValid && isDescriptionValid && isAssignedToValid && isDueDateValid) {
+        const newTask = createTask (name.value, description.value, assignedTo.options[assignedTo.selectedIndex].value, dueDate.value, status.options[status.selectedIndex].value);
+        
+        return newTask;
+    }
+    else
+        return false;
+};
+
+// validate user input on create task form
+const loadUpdateTaskForm = task => {
+
+    let updateForm = document.forms['update-task'];
+
+    const name = document.querySelector('#update-task div div #name');
+    if(name) name.value = task.name;
+    console.log('this is my updateForm ' + updateForm);
+
+    const description = document.querySelector('#description');
+    if(description) document.querySelector('#description').value = task.description;
+
+    const assignedTo = document.querySelector('#assigned-to');
+    if(assignedTo) assignedTo.options[assignedTo.selectedIndex].value = task.assignedTo;
+
+    const dueDate = document.querySelector('#due-date');
+    if(dueDate) document.querySelector('#due-date').value = task.dueDate;
+
+    const status = document.querySelector('#status');
+    if(status) status.options[status.selectedIndex].value = task.status;
+};
+
+// #endregion
+
+
+const taskHtml = createTaskHtml('task1', 'description 1', 'Susanti', '2020-09-18', 'To Do'); 
+console.log(taskHtml);
+
+/************************************ Event Listeners ************************************/
+if(document.querySelector('#add-button')) {
+    document.querySelector('#add-button').addEventListener('click', (event) => {
+        event.preventDefault();
+        window.location.href = 'create-task.html';
+    }, true);
+}
+
+if(document.querySelector('#create-task')) {
+    document.querySelector('#create-task').addEventListener('submit', (event) => {
+        event.preventDefault();
+        
+        const isCreateFormValid = validateTaskForm();
+        if(isCreateFormValid) {
+            window.location.href = 'index.html';
+            TaskManager.addTask(isCreateFormValid);
+            console.log(tasks.length);
+        }
+    }, true);
+}
+
+if(document.querySelector('#homepage')) {
+    document.querySelector('#homepage').addEventListener('load', (event) => {
+        event.preventDefault();
+        taskList.getAllTasks();
+
+    }, true);
+}
+
+if(document.querySelector("#todo-list a")) {
+    document.querySelector("#todo-list a").addEventListener('click', (event) => {
+        event.preventDefault();
+    });
+}
+
+if(document.querySelector(".card-footer button")) {
+    document.querySelector(".card-footer button").addEventListener('click', (event) => {
+        event.preventDefault();
+    });
+}
---
+++ js/task-manager.js
@@ -0,0 +1,178 @@
+/************************************ Task 7 ************************************/
+// #region
+
+// create the html element of a task based on information of the task parameters
+const createTaskHtml = (name, description, assignedTo, dueDate, status) => {
+    const taskItemHtml = 
+    `<a href="#/update-task" class="list-group-item list-group-item-action">
+        <div class="card text-danger border-danger mb-2 shadow">
+            <div class="card-header bg-transparent d-flex justify-content-between">
+                <p>Due Date: <date>${dueDate}</date></p>
+                <p>${status}</p>
+            </div>
+            <div class="card-body">
+                <h5 class="card-title">${name}</h5>
+                <p class="card-text">${description}</p>
+            </div>
+            <div class="card-footer bg-transparent d-flex justify-content-between">
+                <h6>${assignedTo}</h6>
+                <button>Delete</button>
+            </div>
+        </div>
+    </a>`;
+
+    return taskItemHtml;
+}
+
+// #endregion
+
+
+
+/************************************ Task 6 ************************************/
+// #region 
+
+/*************************** Create TaskManager Class ***************************/
+// #region
+
+// Task manager class
+let TaskManager = class  {
+    constructor() {
+        this.tasks = [];
+        this.currentId = this.tasks.length + 1;
+    }
+
+    // functions that add a new task object into the tasks array.   
+    addTask = task => {
+        task.id = 'todo' + this.currentId;
+        this.currentId++;
+        this.tasks.push(task);
+    };
+
+    // function that retrieves all tasks in the Tasks array.
+    getAllTasks = () => this.tasks.map(item => item)
+        .sort((a, b) => { 
+            const dateA = new Date(a.dueDate); 
+            const dateB = new Date(b.dueDate);
+            dateA < dateB ? 1 : -1;        
+        });
+
+    // function that retrieves only tasks with status that matches the selected status. 
+    getAllTasksWithStatus = status => this.tasks.filter(item => { 
+        if(item.status === status) return item; });
+
+    // function that find and delete a selected task.  
+    deleteTask = task => {
+        const selectedIndex = this.tasks.findIndex(item => item.id === task.id);
+        this.tasks.splice(selectedIndex, selectedIndex >= 0 ? 1 : 0);
+        
+        return task;
+    };
+
+    // function that find and update the status of a selected task.  
+    updateTaskStatus = (id, status) => this.tasks.map(item => {
+        if(item.id === id)
+            item.status = status;
+    });
+
+    // function that find and update the asignee of the selected task.  
+    assignTask = (id, assignee) => this.tasks.map(item => {
+        if(item.id === id)
+            item.assignedTo = assignee;
+    });
+
+    // function that find and update a selected task.  
+    updateTask = (id, task) => this.tasks.map(item => {
+        if(item.id === id) {
+            item.name = task.name;
+            item.description = task.description;
+            item.dueDate = task.dueDate;
+            item.assignedTo = task.assignedTo;
+            item.status = task.status;
+        }
+        else 
+            return item;
+    });
+
+    render() {
+        let tasksHtmlList = [];
+        this.tasks.map(task => {
+            const date = newDate(task.dueDate);
+            const formattedDate = date.toISOString().substring(0, 10);
+            const taskHtml = createTaskHtml(task.name, task.description, task.assignedTo, formattedDate, task.dueDate);
+            tasksHtmlList.push(taskHtml);
+        });
+
+        // const tasksHtml 
+    }
+}
+
+// #endregion
+
+
+
+/**************************** Create new task objects ****************************/
+// #region
+
+// "Constructor Function" that creates new task object.
+function Task(name, description, assignedTo, dueDate, status) {
+    this.id = '';
+    this.name = name;
+    this.description = description;
+    this.assignedTo = assignedTo;
+    this.dueDate = dueDate;
+    this.status = status;
+}
+
+// creating task items.
+const task1 = new Task('task 1', 'description 1', 'Susanti', '2020-09-20', 'To Do');
+const task2 = new Task('task 2', 'description 2', 'Nick', '2020-09-15', 'In Progress');
+const task3 = new Task('task 3', 'description 3', 'Lakshmi', '2020-09-18', 'Review');
+const task4 = new Task('task 4', 'description 4', 'Robin', '2020-09-12', 'Done');
+
+//#endregion
+
+
+
+/****************************** Testing Data Manipulation *******************************/
+// #region
+
+/******************** Create an Instance of TaskManager ********************/
+let taskList = new TaskManager();
+
+/******************** CRUD -> Create ********************/
+taskList.addTask(task1);
+taskList.addTask(task2);
+taskList.addTask(task3);
+taskList.addTask(task4);
+// console.log(taskList.tasks);
+
+/******************** CRUD -> Read ********************/
+
+taskList.getAllTasks();
+
+// taskList.getAllTasksWithStatus('To Do');
+// console.log(taskList.getAllTasksWithStatus('To Do'));
+
+/******************** CRUD -> Delete ********************/
+const deleteThisTask = taskList.tasks.find(item => item.id === 'todo2')
+// taskList.deleteTask(deleteThisTask);
+// console.log(taskList.deleteTask(deleteThisTask));
+
+/******************** CRUD -> Update ********************/
+// taskList.updateTaskStatus('todo4', 'Review')
+
+// first create a new task info to replace the value of an exsiting task  
+const updatedTask = new Task();
+updatedTask.name = 'Sprint 3';
+updatedTask.description = 'Replace todo3';
+updatedTask.dueDate = '2020-09-30';
+updatedTask.assignedTo = 'Susanti';
+updatedTask.status = 'To Do';
+taskList.updateTask('todo3', updatedTask);
+// taskList.assignTask('todo1', 'Edison')
+
+// console.log(taskList.getAllTasks());
+
+// #endregion
+
+// #endregion
